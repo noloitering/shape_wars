879d38c775507e9d13384079a61a3c4267ab481e
Eric Barrett <eibarrett@mun.ca>
2021-04-09 05:04:29 -0230

    Lock Framerate
    Although it isn't recommended in the documentation, the game needs to run at a fixed framerate. Will have to look over game logic at a later point

--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@ int main()
 	std::cout << "initializing game" << std::endl;
 #if defined(PLATFORM_WEB)
 	std::cout << "running for web" << std::endl;
-    emscripten_set_main_loop(main_loop, 0, 1);
+	// TODO: fix game logic so that framerate doesn't have to be fixed
+    emscripten_set_main_loop(main_loop, 60, 1);
 #else
 	std::cout << "running for desktop" << std::endl;
     while (!WindowShouldClose())
